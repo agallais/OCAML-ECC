d1102db7cda3dd0a17c7be71f3c9a4ec964f1b25
monstermunch <aymeri.gallais@gmail.com>
2013-05-13 22:58:15 +0300

    Update opérations modulaires.ml
    Là, çà marche. le programme calcule l'inverse de n modulo p. (l'inverse de n dans Z/pZ )

--- opérations modulaires.ml
+++ opérations modulaires.ml
@@ -1,22 +1,35 @@
-(*inversionmodulaire n p: ici p est un nombre premier, et ceci pour des besoin de commodité*)
-let euklide n p =    
-let i= ref n and j = ref p and l = ref [] in
-    while ( !i<>1 && !j<>1) do 
+let classe u p = 
+if u > 0 then (u mod p) else p + (u mod p);;
 
-      l := (!j/!i,!j )::!l;
-      j:= !i;
-      i:=!j mod !i;
+let inversionmodulaire n p =
+if classe n p = 1 then 1 
+else if (n mod p) = 0 then failwith "Element non inversible"
+else
+let i= ref n and j = ref p and c= ref 0 and l = ref [] in
+while ( !i<>1 && !j<>1) do
+l := ((!j/(!i),!j )::!l);
+c:= !i;
+i:=!j mod !i;
+j:=!c;
+done;
+let rec bezout ((r,a),(q,b),l) =
+    match l with
+        | [] -> ((r,a),(q,b))
+        |(k,p)::l2 -> bezout ((q,(b-k*a)),(p,a),l2)
+in 
+let (k,r)::q = !l in
+let (_,u),(_,_)=bezout ((!c,-k),(r,1),q)in 
+classe u p
+;;
 
-    done;
-let rec bezout (r,a) (q,b) l = 
-    match l with 
-        | [] -> (r,a) (q,b)
-        |(k,p)::l2 -> bezout (q, (b- k*a)) (p,a) l2
-        in
-let (k,q)::_ = !l in
-bezout (!j,-k) (q,1) !l ;; (*Le programme bezout cherche à résoudre l'équation diophantienne nu + pv =1, maintenant qu'on
-a fait la suite des divisions de l'algorithme d'Euclide*)
+let test p = 
+for i= 1 to p-1 do 
+if i* inversionmodulaire i p mod p = 1 then ()
+else failwith "erreur"
+done;
+;;
 
-let inversionmodulaire n p = let ((a,u), (_,_))= euklide n p in   u mod p ;; 
-    
-(*Je crois qu'il n'est pas nécessaire de vérifier que a est bien égal à n car l'inégalité r<q est bien vérifiée à chaque étape dans le déroulement du progeramme bezout*)
+inversionmodulaire 1 17;;
+test 23;;
+inversionmodulaire (-1) 2;;
+(-1) mod 2;;
